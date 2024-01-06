0018c422f7f6e31be8c1657f2080dec9843e5655
Mihail Danilov <mihaildanilov793@gmail.com>
2024-01-06 20:13:21 +0200

    feat: move main loop to separate file

--- game/main.py
+++ game/main.py
@@ -1,24 +1,20 @@
 from pygame.locals import *
 import pygame
-import random
 import sys
-import assets.assets as assets
-from entities.enemy import Enemy
-from entities.player import Player
 from screens.credits import credits_screen
 from utils import draw_text, fade, fadeStart, redrawGameWindow
-from mechanics.projectile import Projectile
 from settings import SCREEN_WIDTH
 from init_game import win, mainClock, main_font, comicsans, bg, mainMenuBg, sprites, hitSound
+from game.game_loop import game_loop
 
-
-enemyPick = 1
-score = 0
 bulletsCountLeft = 5
 allBullets = 695
 
-
 def main_menu():
+    enemyPick = 1
+    score = 0
+    bulletsCountLeft = 5
+    allBullets = 695
     fadeStart(win, mainMenuBg, main_font)
     click = False
     main_menuRunning = True
@@ -43,7 +39,7 @@ def main_menu():
         if button_1.collidepoint((mx, my)):
             if click:
                 fade(win)
-                game()
+                enemyPick, score, bulletsCountLeft,allBullets=game_loop(enemyPick, score, bulletsCountLeft,allBullets)
 
         if button_2.collidepoint((mx, my)):
             if click:
@@ -80,152 +76,6 @@ def main_menu():
 
         pygame.display.update()
         mainClock.tick(24)
-
-# mainloop
-
-
-def game():
-    global enemyPick, score, bulletsCountLeft, allBullets, bullets, enemy, man
-    bullets = []
-
-    shootLoop = 0
-
-    # Randomly choosen spawn points within these ranges
-    player_spawn_range = (50, SCREEN_WIDTH // 4)
-    enemy_spawn_range = [SCREEN_WIDTH // 4 + 100, SCREEN_WIDTH - 200]
-    player_x = random.randrange(*player_spawn_range)
-    enemy_x = random.randrange(*enemy_spawn_range)
-    enemyPathInPx = enemy_x + 200
-
-    man = Player(player_x, 528, 64, 64, sprites)
-    # Enemy can be created with the following parameters:
-    # Enemy(x, y, enemy_width, enemy_height,  end, hitbox_width, hitbox_height, enemyType='first',health = 10):
-
-    if enemyPick == 1:
-        enemy = Enemy(enemy_x, 530, 64, 64, enemyPathInPx,
-                      33, 58, sprites, 'first', 10)
-
-    elif enemyPick == 2:
-        enemy = Enemy(enemy_x, 490, 90, 90, enemyPathInPx,
-                      120, 100, sprites, 'second', 20)
-    elif enemyPick == 3:
-        enemy = Enemy(enemy_x, 490, 90, 90, enemyPathInPx,
-                      57, 90, sprites, 'third', 30)
-
-    elif enemyPick == 4:
-        enemy = Enemy(enemy_x, 490, 90, 90,
-                      enemyPathInPx, 80, 100, sprites, 'fourth', 50)
-    elif enemyPick == 5:
-        enemy = Enemy(enemy_x, 490, 90, 90,
-                      enemyPathInPx, 64, 120, sprites, 'fifth', 75)
-    elif enemyPick >= 6:
-        enemy = Enemy(enemy_x, 490, 90, 90,
-                      enemyPathInPx, 64, 120, sprites, 'sixth', 100)
-
-    run = True
-    while run:
-        mainClock.tick(30)
-
-        if score < 0:
-            # win.fill(255,255,255)
-            allBullets = 695
-            enemyPick = 1
-            score = 0
-            fade(win)
-            # pygame.time.delay(3000)
-            run = False
-
-        if enemy.visible == True:
-            if man.hitbox[1] < enemy.hitbox[1] + enemy.hitbox[3] and man.hitbox[1] + man.hitbox[3] > enemy.hitbox[1]:
-                if man.hitbox[0] + man.hitbox[2] > enemy.hitbox[0] and man.hitbox[0] < enemy.hitbox[0] + enemy.hitbox[2]:
-                    man.hit(win)
-                    score -= 5
-
-        else:
-            enemyPick += 1
-            run = False
-
-        if shootLoop > 0:
-            shootLoop += 1
-        if shootLoop > 3:
-            shootLoop = 0
-
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                fade(win)
-                pygame.quit()
-                sys.exit()
-
-        for bullet in bullets:
-            if bullet.y - bullet.radius < enemy.hitbox[1] + enemy.hitbox[3] and bullet.y + bullet.radius > enemy.hitbox[1]:
-                if enemy.visible == True:
-                    if bullet.x + bullet.radius > enemy.hitbox[0] and bullet.x - bullet.radius < enemy.hitbox[0] + enemy.hitbox[2]:
-                        hitSound.play()
-                        enemy.hit(bullets)
-                        score += 1
-                        bulletsCountLeft -= 1
-                        if bullet in bullets:
-                            bullets.remove(bullet)
-
-            if bulletsCountLeft == 0:
-                allBullets -= 5
-                bulletsCountLeft = 5
-            if bullet.x < SCREEN_WIDTH and bullet.x > 0:
-                bullet.x += bullet.vel
-
-            else:
-                bullets.pop(bullets.index(bullet))
-                bulletsCountLeft -= 1
-                bulletsCountLeft = 5
-
-        keys = pygame.key.get_pressed()
-
-        if keys[pygame.K_SPACE] and shootLoop == 0:
-
-            if man.left:
-                facing = -1
-            else:
-                facing = 1
-
-            if len(bullets) < 5:
-                bullets.append(Projectile(round(man.x + man.width // 2),
-                               round(man.y + man.height//2), 3, (255, 255, 255), facing))
-                shootLoop = 1
-
-        if keys[pygame.K_LEFT] and man.x > man.vel:
-            man.x -= man.vel
-            man.left = True
-            man.right = False
-            man.standing = False
-
-        elif keys[pygame.K_RIGHT] and man.x < SCREEN_WIDTH - man.width - man.vel:
-            man.x += man.vel
-            man.right = True
-            man.left = False
-            man.standing = False
-
-        else:
-            man.standing = True
-            man.walkCount = 0
-
-        if not (man.isJump):
-            if keys[pygame.K_UP]:
-                man.isJump = True
-                man.walkCount = 0
-
-        else:
-            if man.jumpCount >= -11:
-                neg = 1
-                if man.jumpCount < 0:
-                    neg = -1
-                man.y -= (man.jumpCount ** 2) * 0.5 * neg
-                man.jumpCount -= 1
-            else:
-                man.isJump = False
-                man.jumpCount = 11
-
-        redrawGameWindow(win,comicsans,score,bulletsCountLeft,allBullets,man,enemy,bullets,bg)
-
 
 main_menu()
 pygame.quit()
