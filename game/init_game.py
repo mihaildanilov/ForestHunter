6e12d47d14d9ced474a632abd04c47768a6b70dd
Mihail Danilov <mihaildanilov793@gmail.com>
2024-01-06 19:20:34 +0200

    refactor: move initialization code to separate file

---
+++ game/init_game.py
@@ -0,0 +1,28 @@
+import pygame
+from settings import SCREEN_WIDTH, SCREEN_HEIGHT
+from assets import assets 
+
+pygame.init()
+
+win = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+icon = assets.load_image("sprites/icon/robe5.png")
+pygame.display.set_icon(icon)
+pygame.display.set_caption("Forest hunter")
+
+main_font = assets.load_fonts()['main_font']
+comicsans = assets.load_fonts()['comicsans']
+
+bg = assets.load_image("sprites/bg/gameBg.png")
+mainMenuBg = assets.load_image("sprites/bg/mainMenuBg.png")
+
+sprites = assets.load_sprites()
+
+mainClock = pygame.time.Clock()
+
+sounds = assets.load_sounds()
+hitSound = sounds['hit_sound']
+music = sounds['background_music']
+pygame.mixer.music.set_volume(0.01)
+hitSound.set_volume(0.01)
+pygame.mixer.music.play(loops=-1)
--- game/main.py
+++ game/main.py
@@ -8,38 +8,14 @@ from entities.player import Player
 from screens.credits import credits_screen
 from utils import draw_text, fade, fadeStart, redrawGameWindow
 from mechanics.projectile import Projectile
-from settings import SCREEN_WIDTH, SCREEN_HEIGHT
+from settings import SCREEN_WIDTH
+from init_game import win, mainClock, main_font, comicsans, bg, mainMenuBg, sprites, hitSound
 
-pygame.init()
-
-win = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
-icon = assets.load_image("sprites/icon/robe5.png")
-pygame.display.set_icon(icon)
-pygame.display.set_caption("Forest hunter")
-
-main_font = assets.load_fonts()['main_font']
-comicsans = assets.load_fonts()['comicsans']
-
-bg = assets.load_image("sprites/bg/gameBg.png")
-mainMenuBg = assets.load_image("sprites/bg/mainMenuBg.png")
-
-sprites = assets.load_sprites()
-
-mainClock = pygame.time.Clock()
-
-sounds = assets.load_sounds()
-hitSound = sounds['hit_sound']
-music = sounds['background_music']
-pygame.mixer.music.set_volume(0.01)
-hitSound.set_volume(0.01)
-pygame.mixer.music.play(loops=-1)
 
 enemyPick = 1
 score = 0
 bulletsCountLeft = 5
 allBullets = 695
-click = False
 
 
 def main_menu():
